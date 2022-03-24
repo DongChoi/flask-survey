b44bf47e354493c5e05b8fd1f66a37f9652aca1f
Dong Hyun Choi <dongandrewchoi@gmail.com>
2022-03-24 15:12:27 -0700

    need to work on question request

--- app.py
+++ app.py
@@ -9,12 +9,20 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 SURVEY_RESPONSES = []
-
+questions_counter = 0
 @app.get('/survey_start')
 def display_start():
     """ Display start page for Survey Start """
 
     instructions = survey.instructions
     title = survey.title
-
     return render_template('survey_start.html', title = title, instructions = instructions)
+
+
+"""make questions html separate per question doing question/0, question/1"""
+@app.get(f'/question/{questions_counter}')
+def display_question(questions_counter):
+
+
+
+    return render_template(f"/question/{questions_counter}", question=survey.questions[questions_counter], choices=survey.questions[questions_counter].choices)
