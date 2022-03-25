340b76a27eb72d6ed1094aec051b0144890ac0d6
Austin Chao <austinschao@gmail.com>
2022-03-24 17:40:52 -0700

    WIP Redirect wrong question

--- app.py
+++ app.py
@@ -2,27 +2,76 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-SURVEY_RESPONSES = []
-questions_counter = 0
+
 @app.get('/survey_start')
 def display_start():
     """ Display start page for Survey Start """
 
-    instructions = survey.instructions
-    title = survey.title
-    return render_template('survey_start.html', title = title, instructions = instructions)
+    # lines below are handled in a separate route when user clicks the button
+    # instructions = survey.instructions
+    # title = survey.title
+    # #survey = survey
+    return render_template('survey_start.html', survey = survey)
 
 
-"""make questions html separate per question doing question/0, question/1"""
-@app.get(f'/question/{questions_counter}')
-def display_question(questions_counter):
+@app.post('/session_start')
+def create_session():
+    """ Create session cookie and redirect to question page """
+
+    session["question"] = 0
+    current_question = session["question"]
+
+    session["responses"] = []
+    response_list = session["responses"]
+    # could also check len of responses instead
+    return redirect(f"/question/{current_question}")
 
 
 
-    return render_template(f"/question/{questions_counter}", question=survey.questions[questions_counter], choices=survey.questions[questions_counter].choices)
+@app.get('/question/<int:current_question>')
+def display_question(current_question):
+    """ Renders template for next question """
+
+
+    question_num = session["question"]
+
+    #should be redirecting not rendering
+    # could shorten it by injecting survey and pluck choices from jinja on template page
+    # if current question != actual question, redirect them, else render correct pg
+
+    return render_template('question.html', question_num=survey.questions[question_num], choices=survey.questions[question_num].choices)
+
+
+@app.post('/answer')
+def add_response():
+    """ Add response to session responses and increment question counter"""
+
+    response = request.form["answer"]
+    response_list = session["responses"]
+
+
+    response_list.append(response)
+    session["responses"] = response_list
+
+    session["question"] += 1
+    next_question = session["question"]
+
+
+
+    if session["question"] < len(survey.questions):
+        return redirect(f"/question/{next_question}")
+    else:
+        return redirect("/completion")
+
+@app.get('/completion')
+def show_thanks():
+    """ Show the client some thanks """
+
+    return render_template('completion.html')
